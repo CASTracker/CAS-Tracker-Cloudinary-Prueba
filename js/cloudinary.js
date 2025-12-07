4d12c3a471afdb35d5d48a3ff6b4db6de4878244
CASTracker <wmbpmf47jh@privaterelay.appleid.com>
2025-12-06 18:23:16 -0800

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,15 +1,15 @@
 // js/cloudinary.js
 
 const dgcltdq5x = "dgcltdq5x";
-const CAS_Tracker = "CAS_Tracker";
+const CLOUDINARY_UPLOAD_PRESET = "CAS_Tracker";
 
 // Subir archivo a Cloudinary
 async function uploadToCloudinary(file) {
-  const url = `https://api.cloudinary.com/v1_1/${dgcltdq5x}/upload`;
+  const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/upload`;
 
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", CAS_Tracker);
+  formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
   formData.append("folder", "cas-tracker");
 
   const res = await fetch(url, {
