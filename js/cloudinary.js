e442a8a27f7e7e96a65e845a7a49700754b537fc
CASTracker <wmbpmf47jh@privaterelay.appleid.com>
2025-12-06 19:05:53 -0800

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,15 +1,16 @@
 // js/cloudinary.js
 
 const CLOUDINARY_CLOUD_NAME = "dgcltdq5x";
-const CLOUDINARY_UPLOAD_PRESET = "CAS_Tracker"; // ✅ este es el correcto
+const CLOUDINARY_UPLOAD_PRESET = "CAS_Tracker";
 
 async function uploadToCloudinary(file) {
-  const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/upload`;
+  const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/raw/upload`;
 
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
   formData.append("folder", "cas-tracker");
+  formData.append("resource_type", "raw");
 
   const res = await fetch(url, {
     method: "POST",
